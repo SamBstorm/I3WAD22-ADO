80c08ab9fa5de3b67a2138248468a3d17d9d91a0
Samuel Legrain <samuel.legrain@bstorm.be>
2022-12-05 10:23:26 +0100

    Add SqlCommand + ExecuteScalar

---
+++ I3WAD22-ADO-Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace I3WAD22_ADO_Console
+{
+    class Program
+    {
+        static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Theatre-DB;Integrated Security=True";
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    //Paramétrer la commande SQL...
+                    command.CommandText = "SELECT [nom] FROM [Type] WHERE [idType] = 1";
+
+
+                    //Ensuite ouvrir connection
+                    connection.Open();
+
+                    //Enfin exécuter la commande SQL
+
+                    string ticketTypeName = (string)command.ExecuteScalar();
+
+                    Console.WriteLine(ticketTypeName);
+                }
+            }
+        }
+    }
+}
