6d5e1e354bf38b7c5ba92d04f8634f18dd29ea05
Samuel Legrain <samuel.legrain@bstorm.be>
2022-12-05 11:52:46 +0100

    Add ExecuteReader + Offline Mode

--- I3WAD22-ADO-Console/Program.cs
+++ I3WAD22-ADO-Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace I3WAD22_ADO_Console
@@ -8,23 +9,75 @@ namespace I3WAD22_ADO_Console
         static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Theatre-DB;Integrated Security=True";
         static void Main(string[] args)
         {
+            //Pas nécessaire pour ADO.net mais permet d'avoir la console en unicode (pour les € par exemple)
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
+                #region Mode Connecté (J'ai possibilité de contacté mon serveur)
+                #region ExecuteScalar() => Récupération d'une seule information
+                //using (SqlCommand command = connection.CreateCommand())
+                //{
+                //    //Paramétrer la commande SQL...
+                //    command.CommandText = "SELECT [prix] FROM [Type] WHERE [idType] = 1";
+
+
+                //    //Ensuite ouvrir connection
+                //    connection.Open();
+
+                //    //Enfin exécuter la commande SQL
+
+                //    decimal ticketTypePrice = (decimal)command.ExecuteScalar();
+
+                //    Console.WriteLine(ticketTypePrice);
+                //} 
+                #endregion
+                #region ExecuteReader => Récupération de multiple informations (colones ou lignes ou les deux)
+                //using (SqlCommand command = connection.CreateCommand())
+                //{
+                //    //Prépare la commande SQL
+                //    command.CommandText = "SELECT * FROM [type]";
+
+                //    //Exécution de la commande SQL
+                //    connection.Open();
+                //    using (SqlDataReader reader = command.ExecuteReader())
+                //    {
+                //        while (reader.Read())
+                //        {
+                //            Console.WriteLine($"{reader["idType"]}. {reader["nom"]} : {reader["Prix"]} €");
+
+                //            //OU
+
+                //            //Console.WriteLine($"{reader[0]}. {reader[1]} : {reader[2]} €");
+                //        }
+                //    }
+                //}
+                #endregion
+                #endregion
+                #region Mode Déconnecté (Je sais que je risque de perdre la connection - exemple : en mobile - je charge le tout avant utilisation)
                 using (SqlCommand command = connection.CreateCommand())
                 {
-                    //Paramétrer la commande SQL...
-                    command.CommandText = "SELECT [nom] FROM [Type] WHERE [idType] = 1";
+                    //Prépare la commande
+                    command.CommandText = "SELECT * FROM [Type]";
 
+                    //Prépare adaptateur pour remplir mon DataSet ou DataTable
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
 
-                    //Ensuite ouvrir connection
-                    connection.Open();
+                    //Prépare DataSet ou DataTable pour récupérer les données
+                    DataTable table_type = new DataTable();
 
-                    //Enfin exécuter la commande SQL
+                    //Demande à l'adaptateur de remplir le DataTable/DataSet grâce à la commande
+                    adapter.Fill(table_type);   //la méthode Fill() établi lui-même la connection
+                                                //(Pas besoin d'Open(), Close(), ...)
 
-                    string ticketTypeName = (string)command.ExecuteScalar();
-
-                    Console.WriteLine(ticketTypeName);
+                    //Je manipule le DataSet/DataTable grâce à ses propriétés (Tables, Rows, Columns, ... )
+                    foreach (DataRow row in table_type.Rows)
+                    {
+                        Console.WriteLine($"{row["idType"]}. {row["Nom"]} : {row["Prix"]}€");
+                    }
                 }
+
+                #endregion
             }
         }
     }
